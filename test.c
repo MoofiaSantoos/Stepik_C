9406474be50da5082ce80bcc0bdfe19317a2a56e
Mina <beroundou@student.21-school.ru>
2024-02-16 18:56:24 +0300

    send

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int nums[] = {1, 2, 3, 4, 6};
-    for (int i = 0; sizeof(nums) / sizeof(nums[0]); i++) {
-        int length = sizeof(nums) / sizeof(nums[0]);
-        printf("%d", length);
-    }
+   
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int length = sizeof(nums) / sizeof(nums[0]);
 }
