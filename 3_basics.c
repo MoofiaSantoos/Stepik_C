17eb48f8b6be01f3a8364935437d751146a2d3b5
Mina <beroundou@student.21-school.ru>
2024-02-27 17:45:49 +0300

    learning start

---
+++ 3_basics.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int Hello_world(); // Задание 3.1 и Задание 3.2
+// Делаем задание 3.3
+
+
+int main() {
+    Hello_world();
+}
+
+
+
+int Hello_world() {
+    printf("Hello world\nGood bye!\n");
+    return 0;
+}
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-   
-}
-
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int length = sizeof(nums) / sizeof(nums[0]);
-}
